f516a9349c913bbdeac5786af1cb87b3345649a0
VitaliaKr <81364950+VitaliaKr@users.noreply.github.com>
2025-03-23 13:34:59 +0400

    done

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
+    static int number;
+
     public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+        while (true) {
+            number = inputNumber();
+            printResul();
+        }
+
+    }
+
+    public static int inputNumber() {
+        System.out.println("Введите целое число: ");
+        return new Scanner(System.in).nextInt();
+    }
+
+    public static boolean isItChet() {
+        return number % 2 == 0;
+    }
+
+    public static void printResul() {
+        System.out.println(isItChet());
     }
 }
