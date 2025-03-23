563006292732863c9ae9a75fd3030b6720e7fe56
VitaliaKr <81364950+VitaliaKr@users.noreply.github.com>
2025-03-23 13:15:40 +0400

    done

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     static double a = 0;
     static double b = 0;
@@ -15,22 +17,27 @@ public class Main {
 
     private static void divide() {
         //todo дописать логику и печать результата
+        System.out.println("Результат деления чисел чисел: " + (a / b));
     }
 
     private static void diff() {
         //todo дописать логику и печать результата
+        System.out.println("Разница чисел: " + (a - b));
     }
 
     private static void multiply() {
         //todo дописать логику и печать результата
+        System.out.println("Произведение чисел: " + (a * b));
     }
 
     private static void sum() {
         //todo дописать логику и печать результата
+        System.out.println("Сумма чисел: " + (a + b));
     }
 
     private static double inputDouble() {
         //todo дописать логику
-        return 0;
+        System.out.println("Введите число: ");
+        return new Scanner(System.in).nextDouble();
     }
 }
